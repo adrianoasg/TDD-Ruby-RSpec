a08c04b90262428a400c392af096c3622adb11d4
adrianoasg <adrianoasg@hotmail.com>
2019-09-30 10:26:29 -0300

    finished tenting models and controllers

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -27,24 +27,67 @@ RSpec.describe CustomersController, type: :controller do
     end    
 
     describe 'as logged Member' do
+        before do
+            @member = create(:member)
+            @customer = create(:customer)
+        end
+
+        # it 'responds a 200 response' do
+        #     sign_in @member
+
+        #     get :show, params: { id: @customer.id }
+        #     expect(response).to have_http_status "200"
+        # end
+
+        it 'with valid attributes' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+
+            expect{ 
+                post :create, params: { customer: customer_params }
+            }.to change(Customer, :count).by(1)
+        end
+
+        it 'with invalid attributes' do
+            customer_params = attributes_for(:customer, address: nil)
+            sign_in @member
+
+            expect{ 
+                post :create, params: { customer: customer_params }
+            }.not_to change(Customer, :count)
+        end
+
         it 'responds a 200 response' do
-            member = create(:member)
-            customer = create(:customer)
-
-            sign_in member
-
-            get :show, params: { id: customer.id }
+            sign_in @member
+            get :show, params: { id: @customer.id }
             expect(response).to have_http_status "200"
         end
 
         it 'render a :show template' do
-            member = create(:member)
-            customer = create(:customer)
-
-            sign_in member
-
-            get :show, params: { id: customer.id }
+            sign_in @member
+            get :show, params: { id: @customer.id }
             expect(response).to render_template(:show)
+        end
+
+        it 'Flash Notice' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+            post :create, params: { customer: customer_params }
+            # p response
+            expect(flash[:notice]).to match(/successfully created/)
+        end
+
+        it 'Content-Type JSON' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+            post :create, format: :json, params: { customer: customer_params }
+            # p response
+            expect(response.content_type).to eq('application/json')
+        end
+
+        #Shoulda Matchers
+        it 'Route' do
+            is_expected.to route(:get, '/customers').to(action: :index) 
         end
     end
 end
